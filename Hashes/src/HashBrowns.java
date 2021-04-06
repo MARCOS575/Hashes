f0023b4eee4e2604b82eb0e751002ebb63ba144b
marcos.francisco <marcos.francisco@PC04541.sjc.sp.gov.br>
2021-04-06 15:09:10 -0300

    commit inicial

---
+++ Hashes/src/HashBrowns.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class HashBrowns {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> h = new HashSet<String>();
+		h.add("amarelo");
+		h.add("azul");
+		h.add("branco");
+		h.add("verde");
+		
+		System.out.println(h);
+		
+		
+		
+
+	}
+
+}
